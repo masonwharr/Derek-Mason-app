52f5cdf5ea1705c5b86f0dfaabf0f5351e7d9734
Derek J Leong <derekjleong@gmail.com>
2020-02-29 11:52:09 -0500

    modified state between child components

---
+++ src/components/Dereks-part.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Derekspart(props) {
+   return <input value={props.value} />;
+}
+
+export default Derekspart;
--- src/components/Masons-part.js
+++ src/components/Masons-part.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Masonspart(props) {
+   function handleChange(event) {
+      // Here, we invoke the callback with the new value
+      props.onChange(event.target.value);
+   }
+
+   return <input value={props.value} onChange={handleChange} />;
 }
 
-export default App;
+export default Masonspart;
