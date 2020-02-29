983562df39aecfe2a2b5448db86cc0d057d4c82a
Derek J Leong <derekjleong@gmail.com>
2020-02-29 16:29:15 -0500

    added sort useState hook to parent

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Masonspart from "./components/Masons-part";
 
 function Launderer() {
    const [basket, setBasket] = useState([]);
+   const [sort, setSort] = useState("new");
 
    const addLaundry = () => {
       setBasket([
@@ -16,10 +17,23 @@ function Launderer() {
       ]);
    };
 
+   const changeSort = option => {
+      switch (option) {
+         case "a":
+            return setSort("new");
+         case "b":
+            return setSort("color");
+         case "c":
+            return setSort("type");
+         default:
+            return setSort("new");
+      }
+   };
+
    return (
       <div className="App">
-         <Masonspart addLaundry={addLaundry} />
-         <Derekspart basket={basket} />
+         <Masonspart addLaundry={addLaundry} setSort={changeSort} />
+         <Derekspart basket={basket} sort={sort} />
       </div>
    );
 }
--- src/components/Dereks-part.js
+++ src/components/Dereks-part.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Derekspart = ({ basket }) => {
+const Derekspart = ({ basket, sort }) => {
    return (
       <ul>
+         <li>Position By: {sort}</li>
          {basket.map(item => (
             <li key={basket.id}>{basket.value}</li>
          ))}
