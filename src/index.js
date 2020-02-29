4f0b14f1303d9d9bb805a93b2a2d8733180188b4
MasonHarrImproving <mason.harr@improving.com>
2020-02-29 11:13:01 -0500

    Added parts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Derekspart from './Dereks-part';
+import Masonspart from './Masons-part';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
